c1e32d21eeba35c7e434c557ace8e70e2f7f8c48
nitaku <matteo.abrate@gmail.com>
2021-02-23 16:06:38 +0100

    some more examples of gods + tweaked depcition

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,19 @@
 
 	const pantheon = {
 		'Zeus': {
-			depiction: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Jupiter_Smyrna_Louvre_Ma13.jpg",
+			depiction: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Bust_of_Zeus.jpg/440px-Bust_of_Zeus.jpg",
 			description: `Zeus is the sky and thunder god in ancient Greek religion, who rules as king of the gods of Mount Olympus. His name is cognate with the first element of his Roman equivalent Jupiter. His mythology and powers are similar, though not identical, to those of Indo-European deities such as Jupiter, Perkūnas, Perun, Indra, Dyaus and Thor.`
+		},
+		'Hera': {
+			depiction: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Hera_Campana_Louvre_Ma2283.jpg/440px-Hera_Campana_Louvre_Ma2283.jpg",
+			description: `Hera (/ˈhɛrə, ˈhɪərə/; Greek: Ἥρᾱ, Hērā; Ἥρη, Hērē in Ionic and Homeric Greek) is the goddess of women, marriage, family and childbirth in ancient Greek religion and mythology, one of the Twelve Olympians and the sister and wife of Zeus. She is the daughter of the Titans Cronus and Rhea. Hera rules over Mount Olympus as queen of the gods. A matronly figure, Hera served as both the patroness and protectress of married women, presiding over weddings and blessing marital unions. One of Hera's defining characteristics is her jealous and vengeful nature against Zeus' numerous lovers and illegitimate offspring, as well as the mortals who cross her.`
+		},
+		'Alcmene': {
+			description: `In Greek mythology, Alcmene (/ælkˈmiːniː/) or Alcmena (/ælkˈmiːnə/; Ancient Greek: Ἀλκμήνη or Doric Greek: Ἀλκμάνα, Latin: Alcumena means "strong in wrath") was the wife of Amphitryon by whom she bore two children, Iphicles and Laonome. She is best known as the mother of Heracles, whose father was the god Zeus. Alcmene was also referred to as Electryone (Ἠλεκτρυώνην), a patronymic name as a daughter of Electryon.`
+		},
+		'Hercules': {
+			depiction: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Hercules_Farnese_3637104088_9c95d7fe3c_b.jpg/800px-Hercules_Farnese_3637104088_9c95d7fe3c_b.jpg",
+			description: `Heracles (/ˈhɛrəkliːz/ HERR-ə-kleez; Greek: Ἡρακλῆς, Hēraklês, Glory/Pride of Hēra, "Hera"), born Alcaeus (Ἀλκαῖος, Alkaios) (/ælˈsiːəs/) or Alcides (Ἀλκείδης, Alkeidēs) (/ælˈsaɪdiːz/), was a divine hero in Greek mythology, the son of Zeus and Alcmene, and the foster son of Amphitryon. He was a great-grandson and half-brother (as they are both sired by the god Zeus) of Perseus. He was the greatest of the Greek heroes, a paragon of masculinity, the ancestor of royal clans who claimed to be Heracleidae (Ἡρακλεῖδαι), and a champion of the Olympian order against chthonic monsters. In Rome and the modern West, he is known as Hercules, with whom the later Roman emperors, in particular Commodus and Maximian, often identified themselves. The Romans adopted the Greek version of his life and works essentially unchanged, but added anecdotal detail of their own, some of it linking the hero with the geography of the Central Mediterranean. Details of his cult were adapted to Rome as well.`
 		}
 	}
 
@@ -84,7 +95,9 @@
 
 <InfoBox>
 	<InfoBoxHeader title="{$selected_id}" subtitle=""/>
-	<Depiction src={$selection.depiction} positionY="top" size="50%"/>
+	{#if $selection.depiction}
+		<Depiction src={$selection.depiction} positionY="top" aspectRatio="square"/>
+	{/if}
 	<Content>{$selection.description}</Content>
 </InfoBox>
 
